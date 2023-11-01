78508e94b6068ecddb7f81e42a2ae8c9013455f4
Carlos-alay012 <jalaya@miumg.edu.gt>
2023-10-31 21:00:55 -0600

    examen final

---
+++ Examen/src/main/java/com/Examen/Final/ExamenApplication.java
@@ -0,0 +1,16 @@
+package com.Examen.Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		System.out.println("Jairo Ardani Castillo Girón");
+		System.out.println("Juan Carlos Adolfo Alay Antonio");
+		SpringApplication.run(ExamenApplication.class, args);
+	}
+
+}
